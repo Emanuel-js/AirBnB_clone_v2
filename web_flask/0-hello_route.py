1142c044824dd71d6b5b2a49dc69b70f642eedc6
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-12 11:25:09 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""helloBNBN"""
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """hello world method"""
     return ("Hello HBNB!")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+
+app.run(host='0.0.0.0', port=5000)
