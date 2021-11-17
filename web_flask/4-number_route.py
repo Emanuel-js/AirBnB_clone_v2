14889d2af8044e8029d7065bd13a95f826eb4d3d
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-17 22:43:02 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+"""
 from flask import Flask
-
+from flask import abort
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    return ("Hello HBNB!")
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return ("HBNB")
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
-    return ("C {}".format(text.replace("_", " ")))
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python_is_cool(text="is cool"):
-    return ("Python {}".format(text.replace("_", " ")))
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
+    """Displays 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
