c2763c8b5d017ea56ecf8f61a22e60d1fb06e771
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-17 22:47:23 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage, State
-from flask import render_template
+"""List cities by states Module"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """display a HTML the States and cities by states"""
+    all_states = list(storage.all(State).values())
+    return (render_template('8-cities_by_states.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(exceptions):
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-    return render_template("8-cities_by_states.html",
-                           states=states,
-                           cities=cities)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
