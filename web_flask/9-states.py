ee59782199b7aa67353c9b72f1d7f9256481f964
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-17 22:48:43 +0300

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,60 +1,35 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage, State
-from flask import render_template
+"""List cities by states Module"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states_list(id=None):
+    """display States, object found by id State and its cities"""
+    all_states = storage.all(State).values()
+    if id:
+        _id = id
+        id_state = None
+        for state in all_states:
+            if state.id == _id:
+                id_state = state
+                break
+    else:
+        id_state = list(all_states)
+    return (render_template('9-states.html', **locals()))
 
 
 @app.teardown_appcontext
-def teardown(exceptions):
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    state_obj = storage.all("State")
-    states = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    return render_template("7-states_list.html", states=states)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-    return render_template("8-cities_by_states.html",
-                           states=states,
-                           cities=cities)
-
-
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def show_states(id=None):
-    state_obj = storage.all("State")
-    city_obj = storage.all("City")
-    states = list()
-    cities = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    for city, value in city_obj.items():
-        cities.append(value)
-
-    state_id = "State.{}".format(id)
-    if id is not None and state_id not in state_obj:
-        states = None
-    return render_template("9-states.html",
-                           states=states,
-                           cities=cities,
-                           id=id)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
