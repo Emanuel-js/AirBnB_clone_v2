92d574c8cca643205d45efbb9d1c91af49f6ef73
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-17 22:46:02 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-from flask import Flask
-from models import storage, State
-from flask import render_template
+"""List all states Module"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """display a HTML the States"""
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(exceptions):
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    state_obj = storage.all("State")
-    states = list()
-    for state, value in state_obj.items():
-        states.append(value)
-    return render_template("7-states_list.html", states=states)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
